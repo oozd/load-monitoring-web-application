8b2f54045181c3eb39ac6c05735b80d370b1ac09
oozd <oozdemironur@gmail.com>
2024-10-05 19:05:33 +0300

    name change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
+  base: "/load-monitoring/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
