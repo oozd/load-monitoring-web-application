273068dae6060ef29227ae051820d08e9aef6b25
oozd <oozdemironur@gmail.com>
2024-10-05 18:32:36 +0300

    path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: "/load-monitoring/",
   devServer: {
     port: 8080,
     proxy: {
